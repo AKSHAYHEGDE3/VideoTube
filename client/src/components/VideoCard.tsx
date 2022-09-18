b900ae77ac9c0c36a5bfd65feeb2c041742d3c74
unknown <akshayhegde158@gmail.com>
2022-09-18 23:31:08 +0530

    timeTag

--- client/src/components/VideoCard.tsx
+++ client/src/components/VideoCard.tsx
@@ -4,14 +4,17 @@ import '../styles/videoCard.scss'
 const VideoCard = () => {
   return (
     <div className='card'>
-        <img src="https://images5.alphacoders.com/644/thumbbig-644177.webp" alt="" />
-        <div className='info'>
-            <img className='avatar' src="https://images5.alphacoders.com/644/thumbbig-644177.webp" alt="" />
-            <div>
-                <p>Tarak mehta ka oolta chasma</p>
-                <p>name . 2.2M views . 15 hours ago</p>
-            </div>
+      <img src="https://images5.alphacoders.com/644/thumbbig-644177.webp" alt="" />
+      <div className='timeTag'>
+        18:30
+      </div>
+      <div className='info'>
+        <img className='avatar' src="https://images5.alphacoders.com/644/thumbbig-644177.webp" alt="" />
+        <div>
+          <p>Tarak mehta ka oolta chasma</p>
+          <p>name . 2.2M views . 15 hours ago</p>
         </div>
+      </div>
     </div>
   )
 }
